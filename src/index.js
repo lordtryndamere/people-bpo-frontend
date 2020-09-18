5161e9e909fb66d133d6eaabfb34b8de4f3c5c88
unknown <kevin.feo@educar.com.co>
2020-09-17 20:05:32 -0500

    haciendo he

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.slim';
+import 'bootstrap/dist/js/bootstrap';
 
 ReactDOM.render(
   <React.StrictMode>
